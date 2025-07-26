6e24fec6dc908ed8305a8677f439e50d25aca184
bzy <1131064363@qq.com>
2025-07-26 22:19:54 +0800

    add blog

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753250357',resources: [
+    cacheName: 'chirpy-1753539481',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
